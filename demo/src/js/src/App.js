8a82e0200317372fab11e8153e5dc71217f97a21
Goetz Botterweck <botterweck@gmail.com>
2022-03-16 18:16:14 +0000

    lesson10 - Adding React state

--- demo/src/js/src/App.js
+++ demo/src/js/src/App.js
@@ -4,12 +4,49 @@ import {getAllStudents} from './client';
 
 class App extends Component {
 
+    state = {
+        students: []
+    }
+
+    componentDidMount() {
+        this.fetchStudents();
+    }
+
+    fetchStudents = () => {
+        getAllStudents()
+            .then(res => res.json()
+                .then(students => {
+                        console.log(students);
+                        this.setState({
+                            students
+                        });
+                    }
+                )
+            );
+    }
+
+
     render() {
-        getAllStudents().then(res => res.json().then(students => {
-            console.log(students);
-        }));
+        const {students} = this.state;
+
+        if (students && students.length) {
+            return (
+                students.map((student, index) => {
+                    return (
+                        <div key={index}>
+                            <h2>{student.studentId}</h2>
+                            <p>{student.firstName}</p>
+                            <p>{student.lastName}</p>
+                            <p>{student.gender}</p>
+                            <p>{student.email}</p>
+                        </div>
+                    );
+                })
+            );
+        }
+
         return (
-            <h1>Hello World!!</h1>
+            <h1>No students found.</h1>
         );
     }
 
