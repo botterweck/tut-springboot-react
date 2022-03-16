9f5900a108a7c1f5a8526b241b8530dd41559ecd
Goetz Botterweck <botterweck@gmail.com>
2022-03-16 17:58:50 +0000

    lesson09 - GET request from frontend to backend

--- demo/src/js/src/App.js
+++ demo/src/js/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {getAllStudents} from './client';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     getAllStudents().then(res => res.json().then(students => {
+        console.log(students);
+    }));
+
+    return (
+        <h1>Hello World!</h1>
+    );
 }
+
+/*function App() {
+    return(
+        <>
+        getAllStudents().then(res => res.json()).then(students => {
+            console.log(students);
+        }))
+        <h1>Hello World!</h1>
+        </>
+    );
+}
+*/
+
 
 export default App;
