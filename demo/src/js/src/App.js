d650759f2231e38035515ffa39838cc1dcd7f2ae
Goetz Botterweck <botterweck@gmail.com>
2022-03-16 18:01:28 +0000

    changed app from function to class, removed unused logo

--- demo/src/js/src/App.js
+++ demo/src/js/src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 import {getAllStudents} from './client';
 
+class App extends Component {
+
+    render() {
+        getAllStudents().then(res => res.json().then(students => {
+            console.log(students);
+        }));
+        return (
+            <h1>Hello World!!</h1>
+        );
+    }
+
+}
+
+/*
 function App() {
      getAllStudents().then(res => res.json().then(students => {
         console.log(students);
@@ -11,18 +25,6 @@ function App() {
         <h1>Hello World!</h1>
     );
 }
-
-/*function App() {
-    return(
-        <>
-        getAllStudents().then(res => res.json()).then(students => {
-            console.log(students);
-        }))
-        <h1>Hello World!</h1>
-        </>
-    );
-}
 */
-
 
 export default App;
